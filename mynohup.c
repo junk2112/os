196565be39b6269cf173c6b938c32f695ae4c0b9
andrew <andrew@packardbell.(none)>
2013-03-18 16:44:59 +0400

    123

---
+++ mynohup.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+int main (int arc, char **argv) 
+{
+  if (arc <= 1) 
+    printf("There are not any args\n");
+  else
+  {
+    char *path = malloc(sizeof(char)*256);  
+    strcpy(path, argv[1]);
+    //printf("path is %s\n",path);
+    int pid;
+    char **a = ++argv;
+    if (pid = fork() == 0)
+    {
+      execve(path, a, NULL);
+    }
+    waitpid(pid, NULL, NULL);
+    free(path);
+  }
+  return 0;
+}
