0f9885608e4c43effe1fee0906423309c6d39342
andrew <andrew@packardbell.(none)>
2013-04-09 12:06:33 +0400

    final version

--- cpulimit.c
+++ cpulimit.c
@@ -3,46 +3,41 @@
 #include <string.h>
 #include <stdlib.h>
 //input: percentages, path, args
-const int checkTime = 10;
-int main (int arc, char **argv) 
+const int checkTime = 1000;
+int main (int arc, char **argv, char **env) 
 {
-  if (arc <= 1)
-    printf("There are not any args\n");
+  if (arc <= 2)
+    printf("There must be 2 args at least: seconds and path\n");
   else
   {
-    float limitPercent = (float)atoi(argv[1])/100;
-    char *path = malloc(sizeof(char)*256);  
-    strcpy(path, argv[2]);
-    printf("path is %s\n",path);
-    printf("percent is %f\n", limitPercent);
+    int limitPercent = atoi(argv[1]);
+    printf("path is %s\n",argv[2]);
+    printf("Use %d percentages of cpu\n", limitPercent);
     int pid;
-    char **args = ++argv;
-    args = ++args;
     
-    if (pid = fork() == -1)
+    if ( (pid = fork()) == -1)
     {	
       printf("fork error\n");
       return 1;
     }
     else
       {
-	if (pid == 0)
+	if (pid == 0)//child
 	{
-	  execve(path, args, NULL);
+	  execv(argv[2], argv+2);
 	}
-	else 
+	else //parent
 	  {
-	    float percent = 0;
 	    while (kill(pid, 0) != -1)
-	    {	    
-	      kill(pid, SIGSTOP);
-	      usleep(checkTime*percent);
-	      kill(pid, SIGCONT);
-	    }
-	    
+	      {	
+		kill(pid, SIGSTOP);
+		usleep(checkTime*(100-limitPercent));
+		kill(pid, SIGCONT);
+		usleep(checkTime*(limitPercent));	
+	      }  
 	  }
-      waitpid(pid, NULL, NULL);
-     }
+	waitpid(pid, NULL, NULL);
+	}
   }
   return 0;
 }
